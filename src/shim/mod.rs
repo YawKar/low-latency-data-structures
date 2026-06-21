abed1149b5cb04192817ad2c721b6ab5e0938067
yawkar <yawkarpov@gmail.com>
2026-06-21 05:47:15 +0200

    refactor(shim): remove unused

--- src/shim/mod.rs
+++ src/shim/mod.rs
@@ -11,26 +11,9 @@ pub mod cell {
     #[derive(Debug)]
     pub(crate) struct UnsafeCell<T>(std::cell::UnsafeCell<T>);
 
-    #[allow(dead_code)]
     impl<T> UnsafeCell<T> {
-        pub(crate) fn new(data: T) -> UnsafeCell<T> {
-            UnsafeCell(std::cell::UnsafeCell::new(data))
-        }
-
-        pub(crate) fn with<R>(&self, f: impl FnOnce(*const T) -> R) -> R {
-            f(self.0.get())
-        }
-
         pub(crate) fn with_mut<R>(&self, f: impl FnOnce(*mut T) -> R) -> R {
             f(self.0.get())
-        }
-
-        pub(crate) fn get_mut(&mut self) -> &mut T {
-            self.0.get_mut()
-        }
-
-        pub(crate) fn get(&self) -> *mut T {
-            self.0.get()
         }
     }
 }
