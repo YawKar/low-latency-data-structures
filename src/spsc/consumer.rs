990a02982b405459aab6cee9b183ce7308674d21
yawkar <yawkarpov@gmail.com>
2026-06-22 11:11:43 +0200

    optimize: make 'ptr()' static dispatch without additional step to differentiate between kinds of allocations

--- src/spsc/consumer.rs
+++ src/spsc/consumer.rs
@@ -1,6 +1,7 @@
 use std::marker::PhantomData;
 
-use crate::shim::cell::Cell;
+use crate::mem::Allocation;
+use crate::shim::cell::{Cell, UnsafeCell};
 use crate::shim::sync::Arc;
 use crate::shim::sync::atomic::AtomicUsize;
 use crate::spsc::queue::Queue;
@@ -11,19 +12,24 @@ pub(super) struct ConsumerState {
     pub cached_tail: Cell<usize>,
 }
 
-pub struct Consumer<T> {
-    inner: Arc<Queue<T>>,
+pub struct Consumer<T, AllocT: Allocation<T>> {
+    inner: Arc<Queue<T, AllocT>>,
     _not_sync: PhantomData<*const ()>,
 }
 
+impl<T> Allocation<T> for () {
+    fn ptr(&self) -> *mut UnsafeCell<std::mem::MaybeUninit<T>> {
+        todo!()
+    }
+}
 // Shouldn't be possible to construct Arc<Consumer<T>> and then use it from different threads as it
 // will break the requirement of *Single* producer *Single* consumer queue.
-static_assertions::assert_not_impl_any!(Consumer<u32>: Sync);
+static_assertions::assert_not_impl_any!(Consumer<u32, ()>: Sync);
 
-unsafe impl<T: Send> Send for Consumer<T> {}
+unsafe impl<T: Send, AllocT: Allocation<T>> Send for Consumer<T, AllocT> {}
 
-impl<T> Consumer<T> {
-    pub(super) fn new(queue: Arc<Queue<T>>) -> Self {
+impl<T, AllocT: Allocation<T>> Consumer<T, AllocT> {
+    pub(super) fn new(queue: Arc<Queue<T, AllocT>>) -> Self {
         Self {
             inner: queue,
             _not_sync: PhantomData,
