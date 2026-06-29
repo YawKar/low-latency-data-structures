d7590678d58a33f1e9dc831b4c1e3a281f99b64b
yawkar <yawkarpov@gmail.com>
2026-06-29 19:35:43 +0200

    feat(spmc): basic impl + tests

---
+++ src/spmc/consumer.rs
@@ -0,0 +1,92 @@
+use std::sync::Arc;
+use std::sync::atomic::{Ordering, fence};
+
+use crate::spmc::queue::Queue;
+
+#[repr(C, align(128))]
+pub(super) struct ConsumerState {
+    read_cursor: usize,
+    cached_write_cursor: usize,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ReadResult<T> {
+    Value(T),
+    Empty,
+    Lapped { skipped: usize },
+}
+
+#[repr(C)]
+pub struct Consumer<T: bytemuck::AnyBitPattern, const CAPACITY: usize, const NCONSUMERS: usize> {
+    state: ConsumerState,
+    inner: Arc<Queue<T, CAPACITY, NCONSUMERS>>,
+}
+
+impl<T, const CAPACITY: usize, const NCONSUMERS: usize> Consumer<T, CAPACITY, NCONSUMERS>
+where
+    T: bytemuck::AnyBitPattern,
+{
+    pub(super) fn new(queue: Arc<Queue<T, CAPACITY, NCONSUMERS>>) -> Self {
+        Self {
+            state: ConsumerState {
+                read_cursor: 0,
+                cached_write_cursor: 0,
+            },
+            inner: queue,
+        }
+    }
+
+    #[inline]
+    pub fn try_read(&mut self) -> ReadResult<T> {
+        if self.state.read_cursor == self.state.cached_write_cursor {
+            // It still may not be empty
+            if self.is_still_empty() {
+                return ReadResult::Empty;
+            }
+        }
+        // SAFETY: it is guaranteed at compile-time that slots has exactly CAPACITY items
+        let slot = unsafe {
+            self.inner
+                .slots
+                .get_unchecked(self.state.read_cursor & (CAPACITY - 1))
+        };
+        // That's the expected seq_no after producer has written the item
+        let expected_seq = self.state.read_cursor.wrapping_mul(2).wrapping_add(2);
+        loop {
+            let seq1 = slot.seq.load(Ordering::Acquire);
+            // If producer currently writes it, we wait (by the way, it means that we were overlapped)
+            if seq1 & 1 == 1 {
+                std::hint::spin_loop();
+                continue;
+            }
+            if seq1 != expected_seq {
+                // We were overlapped => skip everything and go up to the `write_cursor - 1`
+                let new_r_cursor = self.state.cached_write_cursor.wrapping_sub(1);
+                let skipped = new_r_cursor.wrapping_sub(self.state.read_cursor);
+                self.state.read_cursor = new_r_cursor;
+                return ReadResult::Lapped { skipped };
+            }
+            // ASM: prevent the seq1 load from moving below the read
+            fence(Ordering::Acquire);
+            let attempted_read = unsafe { slot.data.get().read_volatile() };
+            // ASM: prevent the seq1 load from moving below the read
+            fence(Ordering::Acquire);
+            let seq2 = slot.seq.load(Ordering::Relaxed);
+            if seq1 == seq2 {
+                self.state.read_cursor = self.state.read_cursor.wrapping_add(1);
+                return ReadResult::Value(attempted_read);
+            }
+            // Torn-read, retry
+        }
+    }
+
+    #[cold]
+    fn is_still_empty(&mut self) -> bool {
+        self.state.cached_write_cursor = self
+            .inner
+            .producer_state
+            .write_cursor
+            .load(Ordering::Acquire);
+        self.state.read_cursor == self.state.cached_write_cursor
+    }
+}
---
+++ src/spmc/queue.rs
@@ -0,0 +1,143 @@
+use std::cell::UnsafeCell;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering, fence};
+
+use crate::spmc::consumer::Consumer;
+use crate::spmc::producer::{Producer, ProducerState};
+
+/// Examples:
+/// ```
+/// # use low_latency_data_structures::spmc::queue::new;
+/// new::<u64, 4, 3>();
+/// ```
+/// Should not compile with queues of capacities other than powers of two:
+/// ```compile_fail
+/// # use low_latency_data_structures::spmc::queue::new;
+/// new::<u64, 3, 1>();
+/// ```
+pub fn new<T, const CAPACITY: usize, const NCONSUMERS: usize>() -> (
+    Producer<T, CAPACITY, NCONSUMERS>,
+    [Consumer<T, CAPACITY, NCONSUMERS>; NCONSUMERS],
+)
+where
+    T: bytemuck::AnyBitPattern,
+{
+    const {
+        assert!(
+            CAPACITY.is_power_of_two(),
+            "Given capacity is not a power of two",
+        );
+    }
+    let q = Arc::new(Queue {
+        producer_state: ProducerState {
+            write_cursor: AtomicUsize::new(0),
+        },
+        slots: std::array::from_fn(|_| Slot {
+            seq: AtomicUsize::new(0),
+            data: UnsafeCell::new(T::zeroed()),
+        }),
+    });
+    let producer = Producer::new(q.clone());
+    let consumers = std::array::from_fn(|_| Consumer::new(q.clone()));
+    (producer, consumers)
+}
+
+pub(super) struct Slot<T: bytemuck::AnyBitPattern> {
+    pub(super) seq: AtomicUsize,
+    pub(super) data: UnsafeCell<T>,
+}
+
+pub(super) struct Queue<T, const CAPACITY: usize, const NCONSUMERS: usize>
+where
+    T: bytemuck::AnyBitPattern,
+{
+    pub(super) producer_state: ProducerState,
+    pub(super) slots: [Slot<T>; CAPACITY],
+}
+
+impl<T, const CAPACITY: usize, const NCONSUMERS: usize> Queue<T, CAPACITY, NCONSUMERS>
+where
+    T: bytemuck::AnyBitPattern,
+{
+    #[inline]
+    pub(super) fn publish(&self, value: T) {
+        let w_pos = self.producer_state.write_cursor.load(Ordering::Relaxed);
+        // Used as both generation control and even-odd guarantee
+        let seq_no = w_pos.wrapping_mul(2);
+        // SAFETY: slots buffer is guaranteed to be the length of CAPACITY items
+        let slot = unsafe { self.slots.get_unchecked(w_pos & (CAPACITY - 1)) };
+        slot.seq.store(seq_no.wrapping_add(1), Ordering::Release);
+        // ARM: prevent the subsequent write from moving above the odd seq store
+        fence(Ordering::Release);
+        unsafe { slot.data.get().write_volatile(value) };
+        slot.seq.store(seq_no.wrapping_add(2), Ordering::Release);
+        self.producer_state
+            .write_cursor
+            .store(w_pos.wrapping_add(1), Ordering::Release);
+    }
+}
+
+#[cfg(test)]
+#[cfg(feature = "tests_basic")]
+mod tests {
+    use super::*;
+    use crate::spmc::consumer::ReadResult;
+
+    #[test]
+    fn single_thread_multiple_consumers_read_messages() {
+        const CAPACITY: usize = 128;
+        const NCONSUMERS: usize = 3;
+        let (producer, consumers) = new::<u64, CAPACITY, NCONSUMERS>();
+        let [mut c1, mut c2, mut c3] = consumers;
+        assert_eq!(c1.try_read(), ReadResult::Empty);
+        assert_eq!(c2.try_read(), ReadResult::Empty);
+        assert_eq!(c3.try_read(), ReadResult::Empty);
+
+        producer.publish(1);
+        assert_eq!(c1.try_read(), ReadResult::Value(1));
+        assert_eq!(c2.try_read(), ReadResult::Value(1));
+        assert_eq!(c3.try_read(), ReadResult::Value(1));
+        assert_eq!(c1.try_read(), ReadResult::Empty);
+        assert_eq!(c2.try_read(), ReadResult::Empty);
+        assert_eq!(c3.try_read(), ReadResult::Empty);
+
+        producer.publish(2);
+        assert_eq!(c1.try_read(), ReadResult::Value(2));
+        assert_eq!(c2.try_read(), ReadResult::Value(2));
+        assert_eq!(c1.try_read(), ReadResult::Empty);
+        assert_eq!(c2.try_read(), ReadResult::Empty);
+
+        producer.publish(3);
+        assert_eq!(c1.try_read(), ReadResult::Value(3));
+        assert_eq!(c1.try_read(), ReadResult::Empty);
+
+        assert_eq!(c2.try_read(), ReadResult::Value(3));
+        assert_eq!(c2.try_read(), ReadResult::Empty);
+
+        assert_eq!(c3.try_read(), ReadResult::Value(2));
+        assert_eq!(c3.try_read(), ReadResult::Value(3));
+        assert_eq!(c3.try_read(), ReadResult::Empty);
+    }
+
+    #[test]
+    fn consumer_can_be_overlapped_by_writer() {
+        const CAPACITY: usize = 8;
+        const NCONSUMERS: usize = 1;
+        let (producer, consumers) = new::<usize, CAPACITY, NCONSUMERS>();
+        let [mut c1] = consumers;
+        assert_eq!(c1.try_read(), ReadResult::Empty);
+
+        // cached_write_cursor will become 7
+        for i in 0..CAPACITY {
+            producer.publish(i);
+        }
+        // read_cursor will become 1
+        assert_eq!(c1.try_read(), ReadResult::Value(0));
+        producer.publish(CAPACITY + 1);
+        producer.publish(CAPACITY + 2);
+        // because `cached_write_cursor - read_cursor = 6`
+        assert_eq!(c1.try_read(), ReadResult::Lapped { skipped: 6 });
+        // because `cached_write_cursor - 1 = 6` and 6 was written into this slot
+        assert_eq!(c1.try_read(), ReadResult::Value(CAPACITY - 1));
+    }
+}
